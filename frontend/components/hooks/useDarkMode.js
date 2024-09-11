2ed5432b1d2a4334e1666dc301745e946d5a22b3
Ness Reliford <nessreliford@gmail.com>
2024-09-11 14:41:12 -0700

    Completed

---
+++ frontend/components/hooks/useDarkMode.js
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import { useLocalStorage } from './useLocalStorage'
+
+export function useDarkMode() {
+    const [dark, setDark] = useLocalStorage('darkmode', false)
+    return [dark, setDark]
+}
